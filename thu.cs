df1d7d0c7100c2024bd98820e779cbd0ac3befdc
MinhDuc003 <huynhminhduc218@gmail.com>
2023-02-27 18:54:45 +0700

    tao them

---
+++ thu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class thu
+    {
+       public void chay()
+        {
+            Console.WriteLine("chao");
+        }
+    }
+}
